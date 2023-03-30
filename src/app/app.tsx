4dfa143d9e018d7de55de3c1fa230cff63f25cf5
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2023-03-30 15:58:20 +0300

    prepare eslint

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <>
-      <NxWelcome title="fancy-web-app" />
-
-      <div />
-    </>
-  );
+  return <div />;
 }
 
 export default App;
